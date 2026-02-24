2dfed6fb4c5b6cf9338d0f96e6b8c6aa5b364ea4
aburiro <96950982+aburiro@users.noreply.github.com>
2026-02-24 22:31:42 +0500

    navigation

---
+++ lib/navigation/app_route.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class AppRoute extends StatefulWidget {
+  const AppRoute({super.key});
+
+  @override
+  State<AppRoute> createState() => AppRouteState();
+}
+
+class AppRouteState extends State<AppRoute> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
